b38ac81231bd0f633920ef09e66b8dd99f6ab9af
emersonpalacio <emersonpalaciootalvaro@gmail.com>
2023-04-29 12:56:30 -0500

    actuli

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,3 +1,16 @@
+// import { createStore, applyMiddleware, compose } from "redux";
+// import { reducer } from "./reducer";
+// import thunk from "redux-thunk";
+
+// export const store = createStore(
+//   reducer,
+//   compose(
+//     applyMiddleware(thunk),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// );
+
+//////////////////////////////////
 import { createStore, applyMiddleware, compose } from "redux";
 import { reducer } from "./reducer";
 import thunk from "redux-thunk";
@@ -6,6 +19,23 @@ export const store = createStore(
   reducer,
   compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : (f) => f
   )
 );
+
+//////////////////////////////////////////////////////////////////
+
+// import { compose, createStore, applyMiddleware } from "redux";
+
+// import reduxThunk from "redux-thunk";
+// import reducers from "./reducers";
+
+// export const store = createStore(
+//   applyMiddleware(reduxThunk),
+//   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+// )(createStore)(reducers);
+
+// //export default store;
